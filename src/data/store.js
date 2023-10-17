0dcbb101d3d9ec690a7e420526812e6476f62af3
silviapetrino <petrino.silvia@gmail.com>
2023-10-17 11:38:15 +0200

    [fix]: store order

--- src/data/store.js
+++ src/data/store.js
@@ -3,9 +3,10 @@ import { reactive } from "vue";
 export const store = reactive({
   apiUrl: 'https://api.themoviedb.org/3/',
   key: '15c65cb183a74c819c4c3483ac688741',
-  searchMovie: 'search/movie?',
-  searchText: "",
   languageDefault: 'it-IT',
+  searchText: "",
+
+  searchMovie: 'search/movie?',
   foundedMovies: [],
   searchTv: 'search/tv?',
   foundTv: []
