5b60fbf83aa1ea8b185cf0f28fc59535c1fe9f59
silviapetrino <petrino.silvia@gmail.com>
2023-10-17 11:37:06 +0200

    [edit]: container to separate movies to tv shows

--- src/components/Main/ContainerCards.vue
+++ src/components/Main/ContainerCards.vue
@@ -39,15 +39,21 @@ export default {
 
 
     <div class="container p-4">
-      <h3 class="fw-semibold"  v-if="getResultMovie.length === 0">Cerca un film per accedere al nostro catalogo</h3>
-      <h3 class="fw-semibold" v-else>Ecco i risultati della tua ricerca:</h3>
-      
-      <div class="row justify-content-between ">
-       
+      <h3 class="fw-semibold"  v-if="getResultMovie.length === 0 & getResultTv.length === 0">Cerca un film per accedere al nostro catalogo</h3>
+      <div class="fw-semibold" v-else>
+        <h3>Ecco i risultati della tua ricerca:</h3>
+
+        <div class="row justify-content-between">
+        <h3 class="fw-semibold">Film:</h3>
         <singolCard v-for="(foundResult, index) in getResultMovie" :key="`movie${index}`" :result="foundResult" />
+        <h3 class="fw-semibold">Serie Tv:</h3>
         <singolCard v-for="(foundResult, index) in getResultTv" :key="`tv${index}`" :result="foundResult" />
      
       </div>
+
+      </div>
+      
+      
     </div>
 
   
